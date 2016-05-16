20be2cd6975a009316465665c2e60d8ef4c5bc59
JUMP <Jump@Jump-MacBook-Air.local>
2016-05-16 17:58:43 +0800

    初版

---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by JUMP on 2016/5/16.
+//  Copyright © 2016年 Jump. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test
+//
+//  Created by JUMP on 2016/5/16.
+//  Copyright © 2016年 Jump. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
